ea7f7be1310886021555c895533b0084a4a545b3
Aakash Sivakumar <saskumar02@gmail.com>
2018-07-09 03:21:18 -0500

    new stuff

--- siteProj/urls.py
+++ siteProj/urls.py
@@ -19,6 +19,6 @@ from oauth.views import home, home2, home3
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', home),
-    url(r'^home2/$', home2),
+    url(r'^hello/$', home2),
     url(r'^home3/$', home3),
 ]
