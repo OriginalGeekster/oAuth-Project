419906a3d016b9cabb43bd98b24f588a37d2ca43
Aakash Sivakumar <saskumar02@gmail.com>
2018-07-09 06:40:07 -0500

    Class-Based View

--- oauth/views.py
+++ oauth/views.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 import random
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views import View
 
 def home(request):
 	num = random.randint(0, 500)
