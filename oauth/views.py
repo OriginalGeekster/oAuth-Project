0e8199207128c70da5b179e350d5647715e3d05c
Aakash Sivakumar <saskumar02@gmail.com>
2018-07-09 02:59:24 -0500

    Initial Commit

---
+++ oauth/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import random
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+	num = random.randint(0, 500)
+	context = {
+		"yeehonk": True, 
+		"num": num,
+	}
+	return render(request, "home.html", context)
+
+def home2(request):
+	
+	return render(request, "home2.html")
+
+def home3(request):
+	
+	return render(request, "home3.html")
